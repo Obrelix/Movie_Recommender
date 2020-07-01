79bea4963e0def8a91588adae6a557f955fc8a7a
Ioannis Alimpertis <ialimpertis@outlook.com>
2020-07-01 13:14:21 +0300

    Initial Commit

---
+++ Movie Recommender/Movie Recommender/OMDBMovie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Recommender
+{
+    public class OMDBMovie
+    {
+        #region Properties
+        #region Private Properties 
+        private string _title;
+        private int _year;
+        private string _rated;
+        private DateTime _released;
+        private string _runtime;
+        #endregion
+        #region Public Properties
+
+        #endregion
+        #endregion
+        public OMDBMovie()
+        {
+
+        }
+    }
+}
